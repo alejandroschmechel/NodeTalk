829a82045461624dc04e0b9aa3066224813dfebf
Alejandro Schmechel <alejandro.schmechel@gmail.com>
2014-06-18 20:45:57 -0300

    Initial server config done, added html folder for frontend

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+var colors = require('colors'),
+	express = require('express'),
+	socket = require('socket.io'),
+	mongo = require('mongodb'),
+	http = require('http');
+ 
+var app = express();
+var server = app.listen(8080);
+var io = require('socket.io').listen(server); 
 
+io.sockets.on("connection", function(client){
+ console.log("\nClient connected!\n".yellow);
+});
+ 
+console.log("\nApplication Started\n".red);
