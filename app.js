6c89b22135e5a7f1c5677d36c2e578647cfd5717
Alejandro Schmechel <alejandro.schmechel@gmail.com>
2014-06-18 23:39:58 -0300

    Alpha chat working fine! Improvements to be done...

--- app.js
+++ app.js
@@ -9,7 +9,19 @@ var server = app.listen(8080);
 var io = require('socket.io').listen(server); 
 
 io.sockets.on("connection", function(client){
- console.log("\nClient connected!\n".yellow);
+ var nickname;
+ client.on('message', function (data) {
+	var date = new Date();
+	var hour = date.getHours();
+	var min = date.getMinutes();
+	io.emit("newmessage", "["+hour+":"+min+"] " + nickname + " Wrote: " + data);
+ });
+
+ client.on('join', function(name) {
+	nickname = name;
+	console.log("Client ".yellow+name.blue+" Connected".yellow);
+ });
+
 });
  
 console.log("\nApplication Started\n".red);
